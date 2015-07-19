a9ebdf85e952c39cac54e1a9ff8a26fa0ea0effe
iurdan <iurdan9@gmail.com>
2015-07-19 16:55:37 +0200

    Simplifies Input.hs

--- src/Input.hs
+++ src/Input.hs
@@ -2,33 +2,24 @@ module Input where
 
 import Life
 import Data.Char
-import Control.Monad (forM_)
 import Data.Array.Repa (Z(..),(:.)(..),computeP,traverse,extent)
 
 -- Life 1.06 text format input
 
--- Make pairs of coordinates
-pairs :: [Int] -> [(Int,Int)]
-pairs [] = []
-pairs [x] = []
-pairs (x:xs) = (x, head xs) : pairs (tail xs)
-
 -- Read cell coordinates
-parseLine :: String -> Maybe [Int]
+parseLine :: String -> Maybe (Int,Int)
 parseLine cs | or (map isNotNum cs) = Nothing
              | length (words cs) /= 2 = Nothing
-             | otherwise = Just $ map readNum (words cs)
+             | otherwise = Just $ (\[a,b] -> (read a, read b)) (words cs)
     where isNotNum c = not (isDigit c) && not (elem c " -\r") 
-          readNum x = read x :: Int
 
 -- Read a pattern file and return a grid with the pattern
 parseFile :: FilePath -> Grid -> IO Grid
 parseFile fp grid = do
   file <- readFile fp
   let (Z :. w :. h) = extent grid
-      ints = map parseLine $ lines file
-      coords = map (fmap pairs) ints
+      coords = map parseLine $ lines file
   computeP $ traverse grid id (\_ sh@(Z :. a :. b) ->
-    if (elem (Just [(a - div w 2, b - div h 2)]) coords)
+    if (elem (Just (a - div w 2, b - div h 2)) coords)
       then 1 
       else 0)
